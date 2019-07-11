7655345bed9c94610a33dca51536466e88190849
Kai Jones <zaksenoj@icloud.com>
2019-07-11 12:55:20 -0700

    add pie parts
    -add lineage version
    -add hidl
    -update libshims

---
+++ hidl/hidl.mk
@@ -0,0 +1,71 @@
+# Audio
+PRODUCT_PACKAGES += \
+    android.hardware.audio@2.0-impl \
+    android.hardware.audio.effect@2.0-impl
+
+# Bluetooth
+PRODUCT_PACKAGES += \
+    hwaddrs \
+    android.hardware.bluetooth@1.0-impl
+
+# Camera
+PRODUCT_PACKAGES += \
+    android.hardware.camera.provider@2.4-impl-legacy \
+    camera.device@1.0-impl-legacy
+
+# Display
+PRODUCT_PACKAGES += \
+    android.hardware.configstore@1.0-service \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.memtrack@1.0-impl
+
+# DRM
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.0-impl
+
+# Lights
+PRODUCT_PACKAGES += \
+    android.hardware.light@2.0-impl
+
+# NET
+PRODUCT_PACKAGES += \
+    android.system.net.netd@1.0 \
+
+# OMX
+PRODUCT_PACKAGES += \
+    android.hardware.media.omx@1.0
+
+# Radio
+PRODUCT_PACKAGES += \
+    android.hardware.radio@1.0-impl
+
+# RenderScript
+PRODUCT_PACKAGES += \
+    android.hardware.renderscript@1.0-impl
+
+# Sensors
+PRODUCT_PACKAGES += \
+    android.hardware.sensors@1.0-impl
+
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.0-impl
+
+# Usb HAL
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0 \
+    android.hardware.usb@1.0-service
+
+# Wifi
+PRODUCT_PACKAGES += \
+    android.hardware.wifi@1.0-service \
+    android.hardware.wifi@1.0-supplicant
+
+# Old binary support
+PRODUCT_PACKAGES += \
+    android.hidl.base@1.0 \
+    android.hidl.manager@1.0
+
--- mt8163-common.mk
+++ mt8163-common.mk
@@ -1,5 +1,6 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+$(call inherit-product, device/common/gps/gps_us_supl.mk)
 PRODUCT_CHARACTERISTICS := tablet
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
